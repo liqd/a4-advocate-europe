1b0b0e33d3576a4ae6da6d3de72e5c6fca4edec3
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-03-15 16:14:47 +0100

    update urls

--- a4_advocate_europe/urls.py
+++ a4_advocate_europe/urls.py
@@ -1,8 +1,33 @@
-from django.conf.urls import include, patterns, url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.views.i18n import javascript_catalog
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
+js_info_dict = {
+    'packages': ('adhocracy4.comments',),
+}
 
-urlpatterns = patterns('',
+urlpatterns = [
+    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls))
+]
 
-                       url(r'^admin/', include(admin.site.urls)),
-                       )
+urlpatterns += i18n_patterns(
+    url(r'', include(wagtail_urls)),
+    url(r'^jsi18n/$', javascript_catalog,
+        js_info_dict, name='javascript-catalog'),
+)
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
