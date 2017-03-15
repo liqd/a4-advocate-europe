5b8611330dc1a18eb1003e98b605b1f6c0cfc014
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-03-15 16:11:20 +0100

    add webpack and linting

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: help
 
 VIRTUAL_ENV ?= .
-SOURCE_DIRS = apps a4_advocate_europe tests
+SOURCE_DIRS = apps a4_advocate_europe cms tests
 
 help:
 	@echo a4-advocate-europe development tools
@@ -19,9 +19,15 @@ help:
 
 install:
 	npm install
+	npm run build
 	if [ ! -f $(VIRTUAL_ENV)/bin/python3 ]; then python3 -m venv .; fi
 	$(VIRTUAL_ENV)/bin/python3 -m pip install --upgrade -r requirements/dev.txt
 	$(VIRTUAL_ENV)/bin/python3 manage.py migrate
+
+watch:
+	trap 'kill %1' KILL; \
+	npm run watch & \
+	$(VIRTUAL_ENV)/bin/python3 manage.py runserver 8000
 
 server:
 	$(VIRTUAL_ENV)/bin/python3 manage.py runserver 8000
@@ -34,3 +40,10 @@ test-lastfailed:
 
 coverage:
 	$(VIRTUAL_ENV)/bin/py.test --reuse-db --cov --cov-report=html
+
+lint:
+	EXIT_STATUS=0; \
+	$(VIRTUAL_ENV)/bin/isort -rc -c $(SOURCE_DIRS) ||  EXIT_STATUS=$$?; \
+	$(VIRTUAL_ENV)/bin/flake8 $(SOURCE_DIRS) --exclude migrations,settings ||  EXIT_STATUS=$$?; \
+	npm run lint --silent ||  EXIT_STATUS=$$?; \
+	exit $${EXIT_STATUS}
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+var BundleTracker = require('webpack-bundle-tracker')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var path = require('path')
+var webpack = require("webpack");
+var autoprefixer = require('autoprefixer');
+
+
+/** How do we use webpack to handle static files?
+ *
+ * - dependencies (js, scss, and css) are installed via npm
+ * - dependencies (js, scss, and css ) are moved to `vendor.(js|css)`
+ *   by specifing them in the vendor entry point
+ * - everything else (our js, scss) is compiled into app.(js|css)
+ * - our images, fonts, icons, js, css and scss is either in each apps
+ *   static folder (/euth/<appname>/static) or in the projects asset folder
+ *   (/euth_wagtail/assets)
+ * - after running webpack all static ressources will be in the
+ *   project static folder (/euth_wagtail/static) and can then be served
+ *   by django
+ * - webpack compiles jsx+es2015 to js and scss to css for us
+ */
+
+module.exports = {
+  entry: {
+    adhocracy4: [
+      './a4_advocate_europe/assets/scss/all.scss',
+      './a4_advocate_europe/assets/js/app.js'
+    ],
+    vendor: [
+      'jquery',
+      'react',
+      'react-dom',
+      'react-flip-move',
+      'react-addons-update',
+      'classnames',
+      'moment',
+      'font-awesome/scss/font-awesome.scss',
+      './a4_advocate_europe/assets/js/jquery-fix.js',
+      'bootstrap-sass',
+      'slick-carousel/slick/slick.min.js',
+      'slick-carousel/slick/slick.css'
+    ]
+  },
+  devtool: 'eval',
+  output: {
+    libraryTarget: 'var',
+    library: '[name]',
+    path: './a4_advocate_europe/static/',
+    publicPath: "/static/",
+    filename: '[name].js'
+  },
+  externals: {
+    'django': 'django'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules\/(?!adhocracy4)/,  // exclude all dependencies but adhocracy4
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-es2015', 'babel-preset-react'].map(require.resolve)
+        }
+      },
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract('style?sourceMap','!css?sourceMap!postcss?sourceMap!sass?sourceMap')
+      },
+      {
+        test: /fonts\/.*\.(svg|woff2?|ttf|eot)(\?.*)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.svg$|\.png$/,
+        loader: 'file-loader?name=images/[name].[ext]'
+      }
+    ]
+  },
+  postcss: [
+    autoprefixer({browsers: ['last 3 versions', 'ie >= 10']})
+  ],
+  resolve: {
+    fallback: path.join(__dirname, 'node_modules'),
+    extensions: ['', '.js', '.jsx', '.scss', '.css']
+  },
+  resolveLoader: { fallback: path.join(__dirname, "node_modules") },
+  sassLoader: {
+    includePaths: [
+      path.resolve('./node_modules/bootstrap-sass/assets/stylesheets')
+    ]
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.js"),
+    new ExtractTextPlugin('[name].css'),
+  ]
+}
