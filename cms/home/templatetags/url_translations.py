5dad64ba62109d2d7d2479f11869ded7cfa2cc59
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-03-31 12:01:59 +0200

    add templatetag to get link in other language

---
+++ cms/home/templatetags/url_translations.py
@@ -0,0 +1,30 @@
+from django import template
+from django.core.urlresolvers import resolve, reverse
+from django.http import Http404
+from django.utils import translation
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def translate_url(context, lang=None, *args, **kwargs):
+    """
+    Get active page's url by a specified language
+    Usage: {% translate_url 'en' %}
+    """
+    path = context['request'].path
+    cur_language = translation.get_language()
+
+    try:
+        view = resolve(path)
+        translation.activate(lang)
+        url = reverse(
+            view.view_name,
+            args=view.args,
+            kwargs=view.kwargs,
+        )
+    except Http404:
+        url = '/' + lang + '/'
+    finally:
+        translation.activate(cur_language)
+    return url
