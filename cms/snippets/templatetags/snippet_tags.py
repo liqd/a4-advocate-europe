cd7d4c3510653754d48067d8d56045b439ba4c71
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-04-06 16:24:45 +0200

    display subpages in dropdown menu

--- cms/snippets/templatetags/snippet_tags.py
+++ cms/snippets/templatetags/snippet_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils import translation
 
 from cms.snippets.models import NavigationMenu
 
@@ -13,3 +14,12 @@ def load_site_menu(menu_name):
         return menu[0].menu_items.all()
     else:
         return None
+
+
+@register.simple_tag
+def get_translated_field(block, field):
+    current_language = translation.get_language()
+    if block.value[field + '_' + current_language]:
+        return block.value[field + '_' + current_language]
+    else:
+        return block.value[field + '_en']
