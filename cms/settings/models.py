6e7167d06e47ebd8a2191fc84386ba6ad8f46a99
Katharina Albers <katharina.albers@liqd.de>
2017-04-21 09:50:25 +0200

    start autofixtures

--- cms/settings/models.py
+++ cms/settings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
+
 @register_setting
 class SocialMediaSettings(BaseSetting):
     facebook = models.URLField(
