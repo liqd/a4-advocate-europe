403ada5759431f861f47d5d30ff564987b6be4c8
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-04-21 18:37:46 +0200

    add form wizard

--- advocate_europe/settings/base.py
+++ advocate_europe/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'formtools',
     'autofixture',
     'rules.apps.AutodiscoverRulesConfig',
     'allauth',
