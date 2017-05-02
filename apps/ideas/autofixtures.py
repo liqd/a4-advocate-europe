f7b44185bdc4c5942d58437a02b78c8e667f1503
Yves Müller <yves.mueller@liqd.de>
2017-05-02 09:49:56 +0200

    Check permissions for csv export and add button

--- apps/ideas/autofixtures.py
+++ apps/ideas/autofixtures.py
@@ -21,6 +21,9 @@ class IdeaSketchAutoFixture(AutoFixture):
         'idea_pitch': generators.CallableGenerator(fake.text),
         'idea_image': generators.ImageGenerator(sizes=IMAGESIZES),
         'idea_location_specify': generators.CallableGenerator(fake.city),
+        'idea_topics_other': generators.ChoicesGenerator(
+            values=['', 'Cats', 'Traffic', 'Poverty']
+        ),
 
         'partner_organisation_1_name':
             generators.CallableGenerator(fake.company),
@@ -40,25 +43,7 @@ class IdeaCompleteAutoFixture(AutoFixture):
 
     IMAGESIZES = ((400, 200), (600, 400), (600, 300), )
 
-    field_values = {
-        'first_name': generators.CallableGenerator(fake.first_name),
-        'last_name': generators.CallableGenerator(fake.last_name),
-        'organisation_name': generators.CallableGenerator(fake.company),
-        'organisation_city': generators.CallableGenerator(fake.city),
-        'contact_email': generators.CallableGenerator(fake.email),
-        'year_of_registration': generators.CallableGenerator(fake.year),
-
-        'idea_pitch': generators.CallableGenerator(fake.text),
-        'idea_image': generators.ImageGenerator(sizes=IMAGESIZES),
-        'idea_location_specify': generators.CallableGenerator(fake.city),
-
-        'partner_organisation_1_name':
-            generators.CallableGenerator(fake.company),
-        'partner_organisation_2_name':
-            generators.CallableGenerator(fake.company),
-        'partner_organisation_3_name':
-            generators.CallableGenerator(fake.company),
-    }
+    field_values = IdeaSketchAutoFixture.field_values
 
     follow_pk = True
     generate_fk = True
