c792ac99f5efa759525805945e1d559f66f3e594
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-05-24 15:58:15 +0200

    test proposal wizard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import factory
+import pytest
 from pytest_factoryboy import register
 
 from . import factories
@@ -5,3 +7,8 @@ from . import factories
 register(factories.UserFactory)
 register(factories.UserFactory, 'user2')
 register(factories.AdminFactory, 'admin')
+
+
+@pytest.fixture
+def image():
+    return factory.django.ImageField(width=1400, height=1400)
