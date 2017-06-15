f4039829e852b010079426027f30b8dbb4c2a837
fuzzylogic2000 <katharina.albers@liqd.de>
2017-06-15 09:09:42 +0200

    simplified migrations, added actions

--- apps/ideas/admin.py
+++ apps/ideas/admin.py
@@ -18,9 +18,39 @@ def set_visit_camp_false(modeladmin, request, queryset):
 set_visit_camp_false.short_description = 'Disallow to add proposal'
 
 
+def set_community_award_winner_true(modeladmin, request, queryset):
+    queryset.update(community_award_winner=True)
+set_community_award_winner_true.\
+    short_description = 'Set to community award winner'
+
+
+def set_community_award_winner_false(modeladmin, request, queryset):
+    queryset.update(community_award_winner=False)
+set_community_award_winner_false.\
+    short_description = 'Unset community award winner'
+
+
+def set_is_winner_true(modeladmin, request, queryset):
+    queryset.update(is_winner=True)
+set_is_winner_true.short_description = 'Set to winner'
+
+
+def set_is_winner_false(modeladmin, request, queryset):
+    queryset.update(is_winner=False)
+set_is_winner_false.short_description = 'Unset winner'
+
+
 class IdeaAdmin(admin.ModelAdmin):
-    list_display = ['idea_title']
-    ordering = ['idea_title']
-    actions = []
+    list_display = ['idea_title', 'is_proposal', 'visit_camp',
+                    'community_award_winner', 'is_winner']
+    ordering = ['-is_proposal', '-visit_camp', '-is_winner', 'idea_title']
+    actions = [
+        set_visit_camp_true,
+        set_visit_camp_false,
+        set_community_award_winner_true,
+        set_community_award_winner_false,
+        set_is_winner_true,
+        set_is_winner_false
+    ]
 
 admin.site.register(models.Idea, IdeaAdmin)
--- apps/ideas/migrations/0007_remove_old_rename_new_fields.py
+++ apps/ideas/migrations/0007_remove_old_rename_new_fields.py
@@ -7,10 +7,22 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('advocate_europe_ideas', '0007_remove_bools_from_ideasketch_and_proposal'),
+        ('advocate_europe_ideas', '0006_copy_values_to_new_fields'),
     ]
 
     operations = [
+        migrations.RemoveField(
+            model_name='ideasketch',
+            name='visit_camp',
+        ),
+        migrations.RemoveField(
+            model_name='ideasketcharchived',
+            name='visit_camp',
+        ),
+        migrations.RemoveField(
+            model_name='proposal',
+            name='is_winner',
+        ),
         migrations.RenameField(
             model_name='idea',
             old_name='is_winner_tmp',
