27b9bd516a30b6c4f713d2b29fe84e590a86aa20
Yves Müller <yves.mueller@liqd.de>
2017-06-21 10:58:43 +0200

    ADd migrations for reach out field

---
+++ apps/ideas/migrations/0008_change_reach_out_to_text_field.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('advocate_europe_ideas', '0007_remove_old_rename_new_fields'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='idea',
+            name='reach_out',
+            field=models.TextField(blank=True, help_text='What kind of advice, comments or feedback would you like to receive about your idea from others on the platform? (max. 300 characters)', max_length=300, verbose_name='Reach out – get feedback, ideas and inspiration from the Advocate Europe Community!'),
+        ),
+        migrations.AlterField(
+            model_name='ideasketcharchived',
+            name='reach_out',
+            field=models.TextField(blank=True, help_text='What kind of advice, comments or feedback would you like to receive about your idea from others on the platform? (max. 300 characters)', max_length=300, verbose_name='Reach out – get feedback, ideas and inspiration from the Advocate Europe Community!'),
+        ),
+    ]
