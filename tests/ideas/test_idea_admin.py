5d633321d80aa63d8947ab75d8c84c6b15b2a529
fuzzylogic2000 <katharina.albers@liqd.de>
2017-07-12 13:28:58 +0200

    admin is also staff now

--- tests/ideas/test_idea_admin.py
+++ tests/ideas/test_idea_admin.py
@@ -9,17 +9,19 @@ def test_idea_admin_actions(client, admin, idea_sketch_factory,
     is2 = idea_sketch_factory()
     # pr = proposal_factory()
 
-    client.login(email=admin.email, password='password')
+    login = client.login(email=admin.email, password='password')
+    assert login
     change_url = reverse('admin:advocate_europe_ideas_idea_changelist')
 
     data = {
         'action': 'set_visit_camp_true',
-        '_selected_action': [is1, is2]
+        '_selected_action': [is1.pk, is2.pk]
         }
     response = client.post(change_url, data)
 
-    # import pdb; pdb.set_trace()
-    assert response.status_code == 200
-    # assert response.context['user'] == admin
-    # assert is1.visit_camp == True
-    # assert is2.visit_camp == True
+    is1.refresh_from_db()
+    is2.refresh_from_db()
+
+    assert response.status_code == 302
+    assert is1.visit_camp
+    assert is2.visit_camp
