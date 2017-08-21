7256479ac8d1c0f07778dec4f30d696c9e3f9b3d
fuzzylogic2000 <katharina.albers@liqd.de>
2017-08-21 13:32:40 +0200

    tag to concatenate strings

--- apps/contrib/templatetags/contrib_tags.py
+++ apps/contrib/templatetags/contrib_tags.py
@@ -20,3 +20,12 @@ def combined_url_parameter(request_query_dict, **kwargs):
         combined_query_dict.setlist(key, [kwargs[key]])
     encoded_parameter = '?' + combined_query_dict.urlencode()
     return encoded_parameter
+
+
+@register.assignment_tag
+def concat_strings(*args):
+    """concatenate strings"""
+    concat_str = ''
+    for string in args:
+        concat_str += str(string)
+    return concat_str
