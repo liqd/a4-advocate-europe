4a7b1a2b61d9d647f3c93bf7f30bd01f730207f7
Yves Müller <yves.mueller@liqd.de>
2017-08-28 13:57:02 +0200

    Add django debug toolbar again

--- advocate_europe/settings/dev.py
+++ advocate_europe/settings/dev.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -9,15 +10,17 @@ for template_engine in TEMPLATES:
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'b*1ljsb!x7@d_o$sohx-&q-7n*#r=lwhy542zxk(e=fj%ey3xp'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-try:
-    import debug_toolbar
-except ImportError:
-    pass
-else:
-    INTERNAL_IPS = ('127.0.0.1', 'localhost')
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar.apps.DebugToolbarConfig',
+]
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+INTERNAL_IPS = ('127.0.0.1', 'localhost')
 
 try:
     from .local import *
