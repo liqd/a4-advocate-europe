06f16e1e486d72a4e248e8214f4cc0f121985d8e
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-08-28 15:16:54 +0200

    add image app

---
+++ cms/images/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from wagtail.wagtailimages.models import (AbstractImage, AbstractRendition,
+                                          Image)
+
+
+class CustomImage(AbstractImage):
+
+    caption = models.CharField(max_length=255, blank=True)
+
+    admin_form_fields = Image.admin_form_fields + (
+        'caption',
+    )
+
+
+class CustomRendition(AbstractRendition):
+    image = models.ForeignKey(CustomImage, related_name='renditions')
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter_spec', 'focal_point_key'),
+        )
