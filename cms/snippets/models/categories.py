0279f1f4c1fbd72f909981b494c2e1faff9e50df
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-08-28 15:23:06 +0200

    refactoring: remove image from category as the field is not needed

--- cms/snippets/models/categories.py
+++ cms/snippets/models/categories.py
@@ -1,21 +1,16 @@
 from django.db import models
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from cms.contrib import translations
 
 
 class Category(models.Model):
     name_en = models.CharField(max_length=255)
     name_de = models.CharField(max_length=255, blank=True, null=True)
-    icon = models.ForeignKey(
-        'wagtailimages.Image', null=True, blank=True,
-        on_delete=models.SET_NULL, related_name='+'
-    )
+
     translated_name = translations.TranslatedField('name')
     panels = [
         FieldPanel('name_en'),
-        FieldPanel('name_de'),
-        ImageChooserPanel('icon'),
+        FieldPanel('name_de')
     ]
 
     def __str__(self):
