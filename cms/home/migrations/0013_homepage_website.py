d54857b2f64693afe4aca2a5a2a7af510e3a54a5
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-08-29 13:46:48 +0200

    rename migration

--- cms/home/migrations/0013_homepage_website.py
+++ cms/home/migrations/0013_homepage_website.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms_home', '0008_make_description_en_optional'),
+        ('cms_home', '0012_rename_new_image_field'),
     ]
 
     operations = [
