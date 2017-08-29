25a70e2e938883dd2d33f584adc38529f9f7ec3e
mkind <sascha.zinke@splone.com>
2017-08-29 15:45:10 +0200

    adding creator notification

--- apps/notifications/emails.py
+++ apps/notifications/emails.py
@@ -8,3 +8,16 @@ class SubmitNotification(emails.UserNotification):
         context = super().get_context()
         context['idea'] = self.object
         return context
+
+
+class NotifyCreatorEmail(emails.UserNotification):
+    template_name = 'advocate_europe_notifications/emails/notify_creator'
+    user_attr_name = 'actor'
+
+    def get_receivers(self):
+        action = self.object
+        if hasattr(action.target, 'creator'):
+            creator = action.target.creator
+            if not creator == action.actor:
+                return [creator]
+        return []
