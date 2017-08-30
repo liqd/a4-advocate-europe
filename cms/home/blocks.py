d8cc7fc45b0fce21b4200996cf225464cdc33708
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-08-30 18:59:01 +0200

    add title to block

--- cms/home/blocks.py
+++ cms/home/blocks.py
@@ -137,6 +137,7 @@ class ImageWithLinkBlock(StructBlock):
 
 
 class ThreeImagesBlock(StructBlock):
+    title = CharBlock(required=False)
     image_left = ImageWithLinkBlock(required=False)
     image_middle = ImageWithLinkBlock(required=False)
     image_right = ImageWithLinkBlock(required=False)
