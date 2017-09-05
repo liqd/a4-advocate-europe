123bcbb84ddd996038aa8a7744a9503cede0d03b
Yves Müller <yves.mueller@liqd.de>
2017-09-05 07:57:15 +0200

    Add docstring to force_lanugage template parser

--- apps/contrib/templatetags/force_translation.py
+++ apps/contrib/templatetags/force_translation.py
@@ -15,17 +15,24 @@ class TransNode(Node):
 
 
 def force_translation(parser, token):
+    """ Parse force_translation template tag.
+
+    .. code:: django
+      {% force_translation 'en' %}
+          {{ _('Will always be english') }}
+      {% endforce_translation %}
+    """
     try:
         nodelist = parser.parse(('endforce_translation',))
         parser.delete_first_token()
         tag_name, lc = token.split_contents()
     except ValueError:
         raise TemplateSyntaxError(
-            '%r tag requires arguments'.format(token.contents.split()[0])
+            '{} tag requires arguments'.format(token.contents.split()[0])
         )
     if not (lc[0] == lc[-1] and lc[0] in ('"', "'")):
         raise TemplateSyntaxError(
-            '%r locale should be in quotes'.format(tag_name)
+            '{} locale should be in quotes'.format(tag_name)
         )
     return TransNode(nodelist, lc[1:-1])
 
