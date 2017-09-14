3c4a653c184772ed6fe98cbd7a1787daa5f7737e
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-09-14 15:23:18 +0200

    add test for is_innovator property

--- tests/users/test_users_models.py
+++ tests/users/test_users_models.py
@@ -11,3 +11,14 @@ def test_fallback_avatar(user):
 def test_avatar_or_fallback_url(user):
     avatar_path = '/static/images/avatars/avatar-0{}.svg'.format(user.pk % 5)
     assert user.avatar_or_fallback_url() == avatar_path
+
+
+@pytest.mark.django_db
+def test_is_innovator(user, idea_sketch_factory):
+    assert not user.is_innovator
+    idea_sketch = idea_sketch_factory(collaborators=[user])
+    assert user.is_innovator
+    idea_sketch.delete()
+    assert not user.is_innovator
+    idea_sketch_factory(creator=user)
+    assert user.is_innovator
