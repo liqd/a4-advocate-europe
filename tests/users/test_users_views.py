8689881b5f820a0f095ed6b92c62d057668f1428
Yves Müller <yves.mueller@liqd.de>
2017-09-20 09:50:05 +0200

    Adapt tests for profile view to new filters

--- tests/users/test_users_views.py
+++ tests/users/test_users_views.py
@@ -11,7 +11,7 @@ from apps.users.forms import UserProfileForm
 @pytest.mark.parametrize('user__username', ['Üß +-@. ö'])
 def test_profile_view(client, user, idea_sketch_factory):
     """
-    Check if profile shows by default only ideas created by user.
+    Check if profile view shows created and collaborated ideas by default.
     """
     idea1 = idea_sketch_factory(creator=user).idea
     idea2 = idea_sketch_factory(collaborators=[user]).idea
@@ -23,23 +23,6 @@ def test_profile_view(client, user, idea_sketch_factory):
     assert response.status_code == 200
     assert response.context_data['view'].user == user
     assert idea1 in response.context_data['idea_list']
-    assert idea2 not in response.context_data['idea_list']
-    assert idea3 not in response.context_data['idea_list']
-
-
-@pytest.mark.django_db
-def test_profile_view_collaboration(client, user, idea_sketch_factory):
-    """
-    Check if profile shows ideas where user collaborated.
-    """
-    idea1 = idea_sketch_factory(creator=user).idea
-    idea2 = idea_sketch_factory(collaborators=[user]).idea
-    idea3 = idea_sketch_factory().idea
-
-    url = reverse('profile', kwargs={'username': user.username})
-    response = client.get(url + '?participation=collaborator')
-    assert response.status_code == 200
-    assert idea1 not in response.context_data['idea_list']
     assert idea2 in response.context_data['idea_list']
     assert idea3 not in response.context_data['idea_list']
 
