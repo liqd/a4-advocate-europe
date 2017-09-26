a00d421cf6b1d7add53d59590f501e056cf15ae8
mkind <sascha.zinke@splone.com>
2017-09-26 18:48:40 +0200

    autofollow creator and collaborators

--- apps/notifications/signals.py
+++ apps/notifications/signals.py
@@ -1,17 +1,32 @@
-from django.db.models.signals import post_save
+from django.db.models import signals
 from django.dispatch import receiver
 
 from adhocracy4.actions.models import Action
 from adhocracy4.actions.verbs import Verbs
 from adhocracy4.comments.models import Comment
 
-from apps.ideas.models import IdeaSketch, Proposal
+from apps.follows import Registry
+from apps.ideas.models import Idea, IdeaSketch, Proposal
 from apps.journeys.models import JourneyEntry
 
 from . import emails
 
 
-@receiver(post_save, sender=Action)
+def _autofollow(instance, pk_set, reverse, enabled):
+
+    # pk_set contains the set of users
+    if not reverse:
+        model = Registry.get_follow_model()
+
+        for pk in pk_set:
+            model.objects.update_or_create(
+                followable=instance,
+                creator_id=pk,
+                defaults={'enabled': enabled}
+            )
+
+
+@receiver(signals.post_save, sender=Action)
 def send_notification(sender, instance, created, **kwargs):
     action = instance
     if action.verb == Verbs.ADD.value:
@@ -27,3 +42,42 @@ def send_notification(sender, instance, created, **kwargs):
         elif (modelcls is JourneyEntry):
             emails.SubmitNotification.send(action.obj)
             emails.NotifyFollowersOnNewJourney.send(action)
+
+
+@receiver(signals.m2m_changed, sender=IdeaSketch.collaborators.through)
+def autofollow_collaborateurs(instance, action, pk_set, reverse, **kwargs):
+    if action == 'post_add':
+        enabled = True
+    elif action in ('post_remove', 'post_clear'):
+        enabled = False
+    else:
+        return
+
+    _autofollow(
+        instance=instance,
+        pk_set=pk_set,
+        reverse=reverse,
+        enabled=enabled
+    )
+
+
+@receiver(signals.pre_save, sender=Idea)
+def autounfollow_creator(sender, instance, **kwargs):
+    old_idea = Idea.objects.filter(pk=instance.pk).first()
+    old_creator = old_idea.creator
+    _autofollow(
+        instance=instance,
+        pk_set=[old_creator.pk],
+        reverse=False,
+        enabled=False
+    )
+
+
+@receiver(signals.post_save, sender=Idea)
+def autofollow_creator(sender, instance, created, **kwargs):
+    _autofollow(
+        instance=instance,
+        pk_set=[instance.creator.pk],
+        reverse=False,
+        enabled=True
+    )
