96aaf0564f9e4e31dbae463e7d8262e51502ab7a
fuzzylogic2000 <katharina.albers@liqd.de>
2017-09-26 09:36:23 +0200

    removed unused template tag get_absolute_uri

--- apps/contrib/templatetags/absolute_url.py
+++ apps/contrib/templatetags/absolute_url.py
@@ -5,10 +5,5 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def get_absolute_uri(context, obj):
-    return context['request'].build_absolute_uri(obj)
-
-
-@register.simple_tag(takes_context=True)
 def get_absolute_uri_static(context, obj):
     return context['request'].build_absolute_uri(static(obj))
--- tests/contrib/test_url_tags.py
+++ tests/contrib/test_url_tags.py
@@ -4,19 +4,6 @@ from adhocracy4.test import helpers
 
 
 @pytest.mark.django_db
-def test_get_absolute_uri(rf):
-    request = rf.get('/')
-    template = ('{% load absolute_url %}'
-                '{% get_absolute_uri obj="/" as absolute_url %}'
-                '{{ absolute_url }}')
-    context = {'request': request}
-    helpers.render_template(template, context)
-
-    assert 'http://testserver/' == context['absolute_url']
-    assert request.build_absolute_uri() == context['absolute_url']
-
-
-@pytest.mark.django_db
 def test_get_absolute_uri_static(rf):
     request = rf.get('/')
     template = ('{% load absolute_url %}'
