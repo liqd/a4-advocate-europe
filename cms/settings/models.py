21e40facdf2283dbac7dcae2df936438bde8cbd3
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-09-26 12:01:08 +0200

    add new field to helppages

--- cms/settings/models.py
+++ cms/settings/models.py
@@ -21,23 +21,34 @@ class HelpPages(BaseSetting):
         'wagtailcore.Page',
         related_name="help_page_communication_camp",
         null=True,
+        blank=True,
         on_delete=models.SET_NULL,
         help_text="Please add a link to the page that explains the communication camp here.")
     annual_theme_help_page = models.ForeignKey(
         'wagtailcore.Page',
         related_name="help_page_annual_theme",
         null=True,
+        blank=True,
         on_delete=models.SET_NULL,
         help_text="Please add a link to the page that explains the annual theme, impact and implementation.")
     terms_of_use_page = models.ForeignKey(
         'wagtailcore.Page',
         related_name="help_page_terms_of_use_page",
+        blank=True,
         null=True,
         on_delete=models.SET_NULL,
         help_text="Please add a link to the page that explains the terms of condition here.")
+    selection_criteria = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name="help_page_selection_criteria",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text="Please add a link to the page that explains the selection criteria here.")
 
     panels = [
         PageChooserPanel('communication_camp_help_page'),
         PageChooserPanel('annual_theme_help_page'),
         PageChooserPanel('terms_of_use_page'),
+        PageChooserPanel('selection_criteria')
     ]
