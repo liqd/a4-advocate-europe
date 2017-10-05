781b3c927aec1a9febbc1ef61714388cd5b804d6
Yves Müller <yves.mueller@liqd.de>
2017-10-05 11:24:44 +0200

    Use newest a4 emails with support for svg logo

--- apps/notifications/emails.py
+++ apps/notifications/emails.py
@@ -1,28 +1,7 @@
-from email.mime.image import MIMEImage
-
-from django.contrib.staticfiles import finders
-
 from adhocracy4 import emails
 
 
-class SVGLogoMixin:
-    """
-    Attaches the static file images/email_logo.svg so it can be used in an html
-    email.
-    """
-    def get_attachments(self):
-        attachments = super().get_attachments()
-        filename = finders.find('images/email_logo.svg')
-
-        if filename:
-            f = open(filename, 'rb')
-            logo = MIMEImage(f.read(), "svg+xml")
-            logo.add_header('Content-ID', '<{}>'.format('logo'))
-            return attachments + [logo]
-        return attachments
-
-
-class SubmitNotification(SVGLogoMixin, emails.UserNotification):
+class SubmitNotification(emails.UserNotification):
     template_name = 'advocate_europe_notifications/emails/submit_notification'
 
     def get_context(self):
@@ -31,7 +10,7 @@ class SubmitNotification(SVGLogoMixin, emails.UserNotification):
         return context
 
 
-class NotifyCreatorEmail(SVGLogoMixin, emails.UserNotification):
+class NotifyCreatorEmail(emails.UserNotification):
     template_name = 'advocate_europe_notifications/emails/notify_creator'
     user_attr_name = 'actor'
 
