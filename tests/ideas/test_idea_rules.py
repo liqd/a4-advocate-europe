1642bb85d81cfa59955ee19ab0d38a5b3e25ded7
fuzzylogic2000 <katharina.albers@liqd.de>
2017-10-11 15:06:57 +0200

    add project_moderator to idea export rule

--- tests/ideas/test_idea_rules.py
+++ tests/ideas/test_idea_rules.py
@@ -1,8 +1,20 @@
 import pytest
 import rules
+
 from apps.ideas import phases
 from tests.factories import UserFactory
 from tests.helpers import active_phase
+
+
+@pytest.mark.django_db
+def test_idea_export_rule(admin, user, module):
+    assert not rules.has_perm('advocate_europe_ideas.export_idea',
+                              user)
+    assert rules.has_perm('advocate_europe_ideas.export_idea',
+                          module.project.moderators.first(),
+                          module)
+    assert rules.has_perm('advocate_europe_ideas.export_idea',
+                          admin)
 
 
 @pytest.mark.django_db
@@ -32,31 +44,32 @@ def test_idea_rate_rules(admin, user, idea_sketch_factory, module):
                                   user,
                                   idea_sketch2)
 
-    @pytest.mark.django_db
-    def test_journey_rules(admin, user, proposal_factory):
-        proposal = proposal_factory()
-        creator = UserFactory()
-        proposal.creator = creator
-        co_worker = UserFactory()
-        proposal.co_workers = [co_worker]
 
-        # proposal not winner
-        assert not rules.has_perm('advocate_europe_ideas.add_journey',
-                                  user, proposal)
-        assert not rules.has_perm('advocate_europe_ideas.add_journey',
-                                  creator, proposal)
-        assert not rules.has_perm('advocate_europe_ideas.add_journey',
-                                  co_worker, proposal)
-        assert rules.has_perm('advocate_europe_ideas.add_journey',
-                              admin, proposal)
+@pytest.mark.django_db
+def test_journey_rules(admin, user, proposal_factory):
+    proposal = proposal_factory()
+    creator = UserFactory()
+    proposal.creator = creator
+    co_worker = UserFactory()
+    proposal.co_workers = [co_worker]
 
-        # proposal is winner!
-        proposal.is_winner = True
-        assert not rules.has_perm('advocate_europe_ideas.add_journey',
-                                  user, proposal)
-        assert rules.has_perm('advocate_europe_ideas.add_journey',
+    # proposal not winner
+    assert not rules.has_perm('advocate_europe_ideas.add_journey',
+                              user, proposal)
+    assert not rules.has_perm('advocate_europe_ideas.add_journey',
                               creator, proposal)
-        assert rules.has_perm('advocate_europe_ideas.add_journey',
+    assert not rules.has_perm('advocate_europe_ideas.add_journey',
                               co_worker, proposal)
-        assert rules.has_perm('advocate_europe_ideas.add_journey',
-                              admin, proposal)
+    assert rules.has_perm('advocate_europe_ideas.add_journey',
+                          admin, proposal)
+
+    # proposal is winner!
+    proposal.is_winner = True
+    assert not rules.has_perm('advocate_europe_ideas.add_journey',
+                              user, proposal)
+    assert rules.has_perm('advocate_europe_ideas.add_journey',
+                          creator, proposal)
+    assert rules.has_perm('advocate_europe_ideas.add_journey',
+                          co_worker, proposal)
+    assert rules.has_perm('advocate_europe_ideas.add_journey',
+                          admin, proposal)
