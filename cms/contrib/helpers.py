6f5261defc79d5503f3b207ad483841a29a1c745
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-10-24 13:42:44 +0200

    allow to add custom link strings to helper function

--- cms/contrib/helpers.py
+++ cms/contrib/helpers.py
@@ -2,12 +2,12 @@ from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from wagtail.wagtailcore.models import Site
 
-from cms.settings.models import HelpPages
+from cms.settings.models import CollaborationCampSettings, HelpPages
 
 LINK_TEXT = _('Please look {}here{} for more information.')
 
 
-def add_link_to_helptext(help_text, help_page_name):
+def add_link_to_helptext(help_text, help_page_name, link_text=None):
     site = Site.objects.filter(
         is_default_site=True
     ).first()
@@ -16,9 +16,18 @@ def add_link_to_helptext(help_text, help_page_name):
     if getattr(help_pages, help_page_name) and \
        getattr(help_pages, help_page_name).live:
         url = getattr(help_pages, help_page_name).url
-        link_text = LINK_TEXT \
+        if not link_text:
+            link_text = LINK_TEXT
+        link_text = link_text \
             .format('<a href="' + url + '" target="_blank">', '</a>')
         return '{} {}'.format(
                 help_text, mark_safe(link_text))
 
     return help_text
+
+
+def get_collaboration_camp_settings():
+    site = Site.objects.filter(
+        is_default_site=True
+    ).first()
+    return CollaborationCampSettings.for_site(site)
