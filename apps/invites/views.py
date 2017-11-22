f5fd05553282354ea395b4de623ff236af4b0192
fuzzylogic2000 <katharina.albers@liqd.de>
2017-11-22 11:21:44 +0100

    display invite invalid instead of 404

--- apps/invites/views.py
+++ apps/invites/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect
+from django.http.response import Http404
+from django.shortcuts import redirect, render
 from django.views import generic
 from rules.compat import access_mixins as mixins
 
@@ -16,12 +17,28 @@ class InviteDetailView(generic.DetailView):
 
     def dispatch(self, request, invite_token, *args, **kwargs):
         if request.user.is_authenticated():
-            return redirect(
-                '{}-update'.format(self.model.__name__.lower()),
-                invite_token=invite_token
-            )
+            try:
+                models.IdeaInvite.objects.get(token=invite_token)
+                return redirect(
+                    '{}-update'.format(self.model.__name__.lower()),
+                    invite_token=invite_token
+                )
+            except models.IdeaInvite.DoesNotExist:
+                return render(
+                    request,
+                    '{}/invite_404.html'.format(self.model._meta.app_label)
+                )
         else:
             return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Http404:
+            return render(
+                request,
+                '{}/invite_404.html'.format(self.model._meta.app_label)
+            )
 
 
 class InviteUpdateView(mixins.LoginRequiredMixin,
