5b615393e0f3fc4270f136dd026d2407cdfee540
Yves Müller <yves.mueller@liqd.de>
2017-12-08 10:11:03 +0100

    Fix liniting errors in files not linted before

--- apps/ideas/static/advocate_europe_ideas/form.js
+++ apps/ideas/static/advocate_europe_ideas/form.js
@@ -1,22 +1,22 @@
 (function ($) {
   var dirtyFlag = 'safeFormDirty'
 
-  var init_dirty_checks = function () {
-    $(document).on('input keyup paste change', 'form[data-safe-form]', function(event) {
+  var initDirtyChecks = function () {
+    $(document).on('input keyup paste change', 'form[data-safe-form]', function (event) {
       var $form = $(this)
       $form.data(dirtyFlag, true)
     })
   }
 
-  var init_safe_exits = function () {
-    $(document).on('click', 'a[data-safe-exit]', function(event) {
+  var initSafeExits = function () {
+    $(document).on('click', 'a[data-safe-exit]', function (event) {
       var $exit = $(this)
       var formName = $exit.data('safeExit')
       var $form = $('form[data-safe-form="' + formName + '"]')
 
       if ($form.data(dirtyFlag)) {
         event.preventDefault()
-        $modal = $form.find('#' + formName + 'ExitModal')
+        var $modal = $form.find('#' + formName + 'ExitModal')
         $modal.find('#save').attr('name', 'next').attr('value', $exit[0].pathname)
         $modal.find('#discard').attr('href', $exit.attr('href'))
         $modal.modal({
@@ -26,7 +26,6 @@
     })
   }
 
-  init_dirty_checks()
-  init_safe_exits()
-
+  initDirtyChecks()
+  initSafeExits()
 })(window.jQuery)
