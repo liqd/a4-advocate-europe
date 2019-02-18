577dcfa21ebddb1b938e091b88fd6b53d42b18b9
fuzzylogic2000 <katharina.albers@liqd.de>
2019-02-18 15:51:15 +0100

    add inline quotes to wagtail's richtext-editor

--- cms/home/wagtail_hooks.py
+++ cms/home/wagtail_hooks.py
@@ -1,6 +1,5 @@
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.admin.rich_text.converters.html_to_contentstate import \
-    BlockElementHandler
+from wagtail.admin.rich_text.converters import html_to_contentstate
 from wagtail.core import hooks
 
 
@@ -22,7 +21,9 @@ def register_blockquote_feature(features):
     )
 
     db_conversion = {
-        'from_database_format': {tag: BlockElementHandler(type_)},
+        'from_database_format': {
+            tag: html_to_contentstate.BlockElementHandler(type_)
+        },
         'to_database_format': {'block_map': {type_: tag}},
     }
 
@@ -30,3 +31,34 @@ def register_blockquote_feature(features):
         'contentstate', feature_name, db_conversion)
 
     features.default_features.append('blockquote')
+
+
+@hooks.register('register_rich_text_features')
+def register_quote_feature(features):
+
+    feature_name = 'quote'
+    type_ = 'quote'
+    tag = 'quote'
+
+    control = {
+        'type': type_,
+        'label': '❝',
+        'description': 'Inlinequote',
+        'style': {'background-color': 'yellow'}
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        'from_database_format': {
+            tag: html_to_contentstate.InlineStyleElementHandler(type_)
+        },
+        'to_database_format': {'style_map': {type_: tag}},
+    }
+
+    features.register_converter_rule(
+        'contentstate', feature_name, db_conversion)
+
+    features.default_features.append('quote')
