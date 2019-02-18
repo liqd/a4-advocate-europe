ab657d8fa40dc7d83c7a0556210c5312f9b90d48
fuzzylogic2000 <katharina.albers@liqd.de>
2019-02-18 14:07:23 +0100

    add field get_newsletters to user model and show in settings and registration

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,11 +1,13 @@
 import crispy_forms as crisp
 from crispy_forms.layout import HTML, Div, Field, Fieldset, Layout, Submit
 from django import forms
+from django.contrib.auth import forms as auth_forms
+from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
 from cms.contrib import helpers
 
-from .models import User
+User = get_user_model()
 
 TERMS_OF_USE_LABEL = _('I accept the terms of use.')
 
@@ -52,7 +54,7 @@ class UserProfileForm(forms.ModelForm):
 class NotificationsForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['get_notifications']
+        fields = ['get_notifications', 'get_newsletters']
 
     @property
     def helper(self):
@@ -61,7 +63,11 @@ class NotificationsForm(forms.ModelForm):
         helper.add_input(Submit('save', 'Save'))
         helper.layout = Layout(
             Fieldset(
-                _('Notifications'),
+                _('Newsletter & Notifications'),
+                Div(
+                    Field('get_newsletters'),
+                    css_class='notification-settings'
+                ),
                 Div(
                     HTML("<p>I want to receive mails with notifications "
                          "about:</p>"),
@@ -73,13 +79,23 @@ class NotificationsForm(forms.ModelForm):
         return helper
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(auth_forms.UserCreationForm):
     terms_of_use = forms.BooleanField(label=TERMS_OF_USE_LABEL)
 
     def signup(self, request, user):
-        pass
+        user.get_newsletters = self.cleaned_data["get_newsletters"]
+
+        user.signup(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['terms_of_use'].label = helpers.add_link_to_helptext(
             self.fields['terms_of_use'].label, "terms_of_use_page")
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2',
+                  'get_newsletters', 'terms_of_use')
--- apps/users/models.py
+++ apps/users/models.py
@@ -105,6 +105,14 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
         default=True
     )
 
+    get_newsletters = models.BooleanField(
+        verbose_name=_('Send me newsletters'),
+        default=False,
+        help_text=_(
+            'Designates whether you want to receive newsletters. '
+            'Unselect if you do not want to receive newsletters.')
+    )
+
     objects = auth_models.UserManager()
 
     USERNAME_FIELD = 'email'
@@ -139,6 +147,13 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
     def get_full_name(self):
         full_name = '%s <%s>' % (self.username, self.email)
         return full_name.strip()
+
+    def signup(self, username, email, commit=True):
+        """Update the fields required for sign-up."""
+        self.username = username
+        self.email = email
+        if commit:
+            self.save()
 
     def get_absolute_url(self):
         return reverse('profile', args=[str(self.username)])
